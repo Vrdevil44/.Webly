9f7b05b21dca186312cdedb939ad2323ec073be0
Vibhu D. <125772875+Vrdevil44@users.noreply.github.com>
2025-11-30 19:10:23 -0800

    Fix GitHub Pages 404: Change build output to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/js/app.js',
   output: {
     filename: 'js/bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   module: {
@@ -53,7 +53,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'docs'),
     },
     compress: true,
     port: 9000,
